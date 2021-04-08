b33da851f938b94bb54824e786b81b14e21ee133
Patrick Bosch <patrick.bosch@uantwerpen.be>
2021-04-08 21:30:46 +0200

    Enable setting a range parameter to a fixed value by having start == stop == step.

--- backtest.py
+++ backtest.py
@@ -518,9 +518,12 @@ def create_config(backtest_config: dict) -> dict:
     config = {k: backtest_config[k] for k in backtest_config
               if k not in {'session_name', 'user', 'symbol', 'start_date', 'end_date', 'ranges'}}
     for k in backtest_config['ranges']:
-        config[k] = tune.choice(np.arange(backtest_config['ranges'][k][0],
-                                          backtest_config['ranges'][k][1] + backtest_config['ranges'][k][2],
-                                          backtest_config['ranges'][k][2]))
+        if backtest_config['ranges'][k][0] == backtest_config['ranges'][k][1] == backtest_config['ranges'][k][2]:
+            config[k] = backtest_config['ranges'][k][0]
+        else:
+            config[k] = tune.choice(np.arange(backtest_config['ranges'][k][0],
+                                              backtest_config['ranges'][k][1] + backtest_config['ranges'][k][2],
+                                              backtest_config['ranges'][k][2]))
     return config
 
 
